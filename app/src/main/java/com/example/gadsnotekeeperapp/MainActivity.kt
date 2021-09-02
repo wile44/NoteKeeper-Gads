23091e150440ec74cc58d5acc595160af8a70c4c
wile44 <goodluckwileonline@gmail.com>
2021-09-02 22:40:31 +0300

    populating contents on the MainActivity

--- app/src/main/java/com/example/gadsnotekeeperapp/MainActivity.kt
+++ app/src/main/java/com/example/gadsnotekeeperapp/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.gadsnotekeeperapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.example.gadsnotekeeperapp.databinding.ActivityMainBinding
 import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.Spinner
 
 
 class MainActivity : AppCompatActivity() {
+
+    private var notePosition = POSITION_NOT_SET
 
     private lateinit var binding: ActivityMainBinding
 
@@ -14,10 +19,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
-        var spinnerAdapter =  ArrayAdapter<CourseInfo>(this,android.R.layout.simple_spinner_item,DataManager.courses.values.toList(),)
+        var spinnerAdapter =  ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,DataManager.courses.values.toList(),)
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        binding.spinnerCourse.adapter = spinnerAdapter
+        var spinner: Spinner = findViewById(R.id.spinnerCourse)
+        spinner.adapter = spinnerAdapter
+
+        notePosition = intent.getIntExtra(NOTE_POSITION, POSITION_NOT_SET)
+
+        if (notePosition != POSITION_NOT_SET) {
+            displayNote()
+        }
+        else {
+            DataManager.notes.add(NoteInfo())
+            notePosition = DataManager.notes.lastIndex
+        }
+    }
+
+    private fun displayNote() {
+        val note = DataManager.notes[notePosition]
+        val noteTitle : EditText = findViewById(R.id.noteText)
+        val noteText : EditText = findViewById(R.id.noteTitle)
+
+        noteTitle.setText(note.title)
+        noteText.setText(note.text)
+
+
+        val coursePosition = DataManager.courses.values.indexOf(note.course)
+        val spinna:Spinner = findViewById(R.id.spinnerCourse)
+
+        spinna.setSelection(coursePosition)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.custom_menu, menu)
+        return true
     }
 }
---
+++ app/src/main/java/com/example/gadsnotekeeperapp/MainNoteListActivity.kt
@@ -0,0 +1,43 @@
+package com.example.gadsnotekeeperapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import com.example.gadsnotekeeperapp.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainNoteListActivity : AppCompatActivity() {
+
+    private var notePosition = POSITION_NOT_SET
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setContentView(R.layout.activity_main_note_list)
+
+//        var fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        var listView: ListView = findViewById(R.id.listView)
+        listView.adapter =  ArrayAdapter(this,android.R.layout.simple_list_item_1,DataManager.notes)
+
+
+
+//        fab.setOnClickListener{ view ->
+//            val activityIntent = Intent(this,MainActivity::class.java)
+//            startActivity(activityIntent)
+//        }
+
+        listView.setOnItemClickListener{ parent, view,position,id ->
+            val activityIntent = Intent(this,MainActivity::class.java)
+            activityIntent.putExtra(NOTE_POSITION,position)
+            startActivity(activityIntent)
+        }
+    }
+
+
+
+}
